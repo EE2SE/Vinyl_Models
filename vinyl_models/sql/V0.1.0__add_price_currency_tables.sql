8a81c1fa4f610442d882a526228a0de48cbda742
EE2SE <147542435+EE2SE@users.noreply.github.com>
2025-04-18 16:47:31 +0100

    depploying first schema to supabase remote

--- vinyl_models/sql/V0.1.0__add_price_currency_tables.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists prod.currency (
-    id serial,
-    symbol text not null,
-    name text not null,
-    create_dt timestamp default now(),
-
-    constraint pk__currency primary key (id),
-    constraint uq__currency__symbol__name unique (symbol, name),
-);
