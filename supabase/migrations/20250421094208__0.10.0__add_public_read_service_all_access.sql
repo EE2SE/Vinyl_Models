3b2dbe8c04fa14a485b5034b59effa4371c0ccfe
EE2SE <147542435+EE2SE@users.noreply.github.com>
2025-04-21 19:42:05 +0100

    sql linter

--- supabase/migrations/20250421094208__0.10.0__add_public_read_service_all_access.sql
+++ supabase/migrations/20250421094208__0.10.0__add_public_read_service_all_access.sql
@@ -9,7 +9,10 @@ GRANT SELECT ON TABLES TO anon;
 
 -- Grant full access to service_role
 GRANT USAGE ON SCHEMA prod TO service_role;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA prod TO service_role;
+GRANT SELECT,
+INSERT,
+UPDATE,
+DELETE ON ALL TABLES IN SCHEMA prod TO service_role;
 
 -- Automatically grant full access to service_role for future tables
 ALTER DEFAULT PRIVILEGES IN SCHEMA prod
